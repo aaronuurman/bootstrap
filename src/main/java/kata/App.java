34c3ae69aa6cef631c2eaabcb7f0cbb00864abef
Lars Eckart <lars.eckart@gmail.com>
2020-06-04 12:13:06 +0300

    gradle wrapper update

--- src/main/java/kata/App.java
+++ src/main/java/kata/App.java
@@ -1,8 +1,20 @@
 package kata;
 
+import java.io.IOException;
+import java.util.jar.Attributes;
+import java.util.jar.Manifest;
+
 class App {
 
-    public static void main(String[] args) {
-        System.out.println("Hello world");
-    }
+  public static void main(String[] args) throws IOException {
+    Attributes stuff = new App().stuff();
+    System.out.println(stuff.getValue("Main-Class"));
+    System.out.println("Hello world");
+  }
+
+  public Attributes stuff() throws IOException {
+    var inputStream = this.getClass().getResourceAsStream("/META-INF/MANIFEST.MF");
+    var manifest = new Manifest(inputStream);
+    return manifest.getMainAttributes();
+  }
 }
