5d3f69cbad6d69f2b33f0991690241c1f9c77859
Lars Eckart <lars.eckart@gmail.com>
2020-08-19 19:08:55 +0300

    remove unwanted code

--- src/main/java/kata/App.java
+++ src/main/java/kata/App.java
@@ -1,20 +1,8 @@
 package kata;
 
 import java.io.IOException;
-import java.util.jar.Attributes;
-import java.util.jar.Manifest;
 
 class App {
 
-  public static void main(String[] args) throws IOException {
-    Attributes stuff = new App().stuff();
-    System.out.println(stuff.getValue("Main-Class"));
-    System.out.println("Hello world");
-  }
-
-  public Attributes stuff() throws IOException {
-    var inputStream = this.getClass().getResourceAsStream("/META-INF/MANIFEST.MF");
-    var manifest = new Manifest(inputStream);
-    return manifest.getMainAttributes();
-  }
+  public static void main(String[] args) throws IOException {}
 }
